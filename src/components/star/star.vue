1f5b58a6ec59f00059166d8f5163c5a35ccfd0e5
Barcode <13302677781@163.com>
2018-05-25 02:33:37 +0800

    version Main

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="star" :class="starType">
-        <span class="star-item" v-for="itemClass in itemClasses" :class="itemClass"></span>
+        <span class="star-item" v-for="(itemClass) in itemClasses" :class="itemClass" :key="itemClass" ></span>
     </div>
 </template>
 
 <script>
-const star_length = 5;
-const Clss_on = 'on';
-const Clss_off = 'off';
-const Clss_half = 'half';
+const STAR_LENGTH = 5;
+const CLSS_ON = 'on';
+const CLSS_OFF = 'off';
+const CLSS_HALF = 'half';
 export default {
   props: {
     size: {
@@ -20,22 +20,23 @@ export default {
   },
   computed: {
     starType() {
-      return 'star-' + this.size;
+      return 'star' + this.size;
     },
-    itemClass() {
+    itemClasses() {
       let result = [];
-      let score = Math.floor(this.score * 2) / 2;//向下取
+      let score = Math.floor(this.score * 2) / 2; // 向下取
       let hasDecimal = score % 1 !== 0;
       let integer = Math.floor(score);
-      for (let i = 0;i < integer; i++) {
-        result.push(Clss_on);
+      for (let i = 0; i < integer; i++) {
+        result.push(CLSS_ON);
       }
       if (hasDecimal) {
-        result.push(Clss_half);
+        result.push(CLSS_HALF);
       }
-      while (result.length < star_length) {
-        result.push(Clss_off);
+      while (result.length < STAR_LENGTH) {
+        result.push(CLSS_OFF);
       }
+      //   console.log(result);
       return result;
     }
   }
@@ -46,44 +47,52 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
 @import "../../common/stylus/mixin.styl"
     .star
-        display block
-        background-repeat no-repeat
-        &.star-48
+        font-size 0
+        text-align center
+        margin-top 10px
+        .star-item
+            display inline-block
+            background-repeat no-repeat
+        &.star48
             .star-item
                 width 20px
                 height 20px
                 margin-right 22px
                 background-size 20px 20px
-            &:last-child
-                margin-right 0
-            &.on
-                bg-img('star48-on')
-            &.half
-                bg-img('star48-half')
-            &.off
-                bg-img('star48-off')
-        &.star-36
+            :last-child
+              margin-right 0
+            .on
+              bg-image('images/star48_on')
+            .half
+              bg-image('images/star48_half')
+            .off
+              bg-image('images/star48_off')
+        &.star36
             .star-item
                 width 15px
                 height 15px
                 margin-right 6px
                 background-size 15px 15px
+            &:last-child
+                margin-right 0
             &.on
-                bg-img('star36-on')
+                bg-image('images/star36_on')
             &.half
-                bg-img('star36-half')
+                bg-image('images/star36_half')
             &.off
-                bg-img('star36-off')
-        &.star-24
+                bg-image('images/star36_off')
+        &.star24
             .star-item
                 width 10px
                 height 10px
                 margin-right 3px
                 background-size 10px 10px
+            &:last-child
+                margin-right 0
             &.on
-                bg-img('star24-on')
+                bg-image('images/star24_on')
             &.half
-                bg-img('star24-half')
+                bg-image('images/star24_half')
             &.off
-                bg-img('star24-off')
+                bg-image('images/star24_off')
 </style>
